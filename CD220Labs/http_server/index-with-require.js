1d1244483be38f289704014a7ede70362a931e1a
Jean-Michel Bodart <jmbodart@skynet.be>
2022-11-30 12:58:11 +0100

    make time-dependent greeting

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -1,9 +1,34 @@
-const http = require('http');
+/*const http = require('http');
 const today = require('./today');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
   res.end(`Hello, World! The date today is ${today.getDate()}`);
+}
+
+const port = 8080;
+const server = http.createServer(requestListener);
+console.log('server listening on port: ' + port);
+server.listen(port);
+*/
+
+const http = require('http');
+const today = require('./today');
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  var dateVal = today.getDate();
+  var greeting = "It is still not morning"
+  if (dateVal.getHours()>6 && dateVal.getHours()<12) {
+    greeting = "Good morning!"
+  } else if (dateVal.getHours()>=12 && dateVal.getHours()<18) {
+    greeting = "Good afternoon!"
+  }else if (dateVal.getHours()>=18 && dateVal.getHours()<21) {
+    greeting = "Good evening!"
+  }else if (dateVal.getHours()>=21 && dateVal.getHours()<24) {
+    greeting = "Good night!"
+  }
+  res.end(`Hello, ${greeting}, the time is: ${dateVal}`);
 }
 
 const port = 8080;
