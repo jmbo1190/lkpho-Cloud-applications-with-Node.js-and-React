867addb1cebd911c894b0c75c870da1d75efd3b8
Jean-Michel Bodart <jmbodart@skynet.be>
2022-12-12 18:04:36 +0100

    Update expressRouting.js
    Add comments how to test the program

--- CD220Labs/expressjs/expressRouting.js
+++ CD220Labs/expressjs/expressRouting.js
@@ -30,3 +30,12 @@ app.listen(3333, () => {
     console.log(`Listening at http://localhost:3333`)
 })
 
+// Test the application
+// 1. Run server
+//    node expressRouting.js   // Listening at http://localhost:3333
+
+// 2. In other terminal:
+//    curl localhost:3333/item/1  //  Item 1 last enquiry Mon Dec 12 2022 18:02:06 GMT+0100 (Central European Standard Time)%
+//    curl localhost:3333/user/1  //  User 1 last successful login Mon Dec 12 2022 18:03:03 GMT+0100 (Central European Standard Time)%
+
+
