78f3ee38febdbcf988bd9942ca292938df199797
Jean-Michel Bodart <jmbodart@skynet.be>
2022-12-12 18:35:14 +0100

    Update expressStaticPages.js
    Add comments how to test application

--- CD220Labs/expressjs/expressStaticPages.js
+++ CD220Labs/expressjs/expressStaticPages.js
@@ -7,3 +7,15 @@ app.listen(3333, () => {
     console.log(`Listening at http://localhost:3333`)
 })
 
+//  Test program:
+//  1. run: node expressStaticPages.js  // Listening at http://localhost:3333
+
+//  2. Open URLs in browser: 
+//     http://localhost:3333/ReactCalc.html
+//     http://localhost:3333/kitten.jpeg
+//     http://localhost:3333/translateToSpanish.html
+
+
+
+
+
